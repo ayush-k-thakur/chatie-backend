7566bfd172e374a81f36669f49af981b3722c9e0
Ayush Kumar Thakur <ayush524425@gmail.com>
2025-06-23 09:24:48 +0545

    first commit

--- socket/socket.js
+++ socket/socket.js
@@ -45,7 +45,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: ["https://chatie-frontend.vercel.app"],
+		origin: [""],
 		methods: ["GET", "POST"],
 		credentials: true,
 	},
