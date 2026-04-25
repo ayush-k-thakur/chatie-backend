03286ed1c1f98737d4423bd58d494aa246c48f85
Ayush Kumar Thakur <144116323+ayush-k-thakur@users.noreply.github.com>
2026-04-25 11:25:13 +0545

    Refactor generateTokenAndSetCookie function
    Removed unnecessary environment checks for cookie settings.

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,18 +1,16 @@
-import jwt from "jsonwebtoken";
-
-const generateTokenAndSetCookie = (userId, res) => {
-	const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-		expiresIn: "15d",
-	});
-
-	const isProduction = process.env.NODE_ENV === "production";
-
-	res.cookie("jwt", token, {
-		maxAge: 15 * 24 * 60 * 60 * 1000,
-		httpOnly: true,
-		sameSite: isProduction ? "none" : "lax",
-		secure: isProduction,
-	});
-};
-
+import jwt from "jsonwebtoken"; 
+ 
+const generateTokenAndSetCookie = (userId, res) => { 
+	const token = jwt.sign({ userId }, process.env.JWT_SECRET, { 
+		expiresIn: "15d", 
+	}); 
+ 
+	res.cookie("jwt", token, { 
+		maxAge: 15 * 24 * 60 * 60 * 1000, // MS 
+		httpOnly: true, // prevent XSS attacks cross-site scripting attacks 
+		sameSite: "none", // CSRF attacks cross-site request forgery attacks 
+		secure: true, 
+	}); 
+}; 
+ 
 export default generateTokenAndSetCookie;
